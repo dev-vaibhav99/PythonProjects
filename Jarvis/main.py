e282185018273c85749c2dcec4c131383021ff14
Vaibhav Chandrashekhar <dev.vaibhav99@gmail.com>
2021-01-11 12:52:02 +0530

    Jarvis can talk now!

--- Jarvis/main.py
+++ Jarvis/main.py
@@ -1,6 +1,17 @@
 import speech_recognition as sr
+import pyttsx3
 
 listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voices', voices[0].id)
+
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
 try:
     with sr.Microphone() as source:
         print("Listening...")
@@ -8,6 +19,7 @@ try:
         command = listener.recognize_google(voice)
         command = command.lower()
         if 'jarvis' in command:
+            talk(command)
             print(command)
 except:
     pass
